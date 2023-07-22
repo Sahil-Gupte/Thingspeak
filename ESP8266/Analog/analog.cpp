d8bb45b836275a62b070a095a01cd5190766f47b
Sahil Gupte <91281882+Sahil-Gupte@users.noreply.github.com>
2023-07-23 01:19:35 +0530

    analog

---
+++ ESP8266/Analog/analog.cpp
@@ -0,0 +1,52 @@
+#include "ThingSpeak.h"
+#include "secrets.h"
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+#include <ESP8266WiFi.h>
+
+char ssid[] = SECRET_SSID;   
+char pass[] = SECRET_PASS;   
+int keyIndex = 0;            
+WiFiClient  client;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  WiFi.mode(WIFI_STA);
+
+  ThingSpeak.begin(client);
+}
+
+void loop() {
+
+ 
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass); 
+      Serial.print(".");
+      delay(5000);
+    }
+    Serial.println("\nConnected.");
+  }
+
+  
+  int valueA0 = analogRead(A0);
+
+  // Write value to Field 1 of a ThingSpeak Channel
+  int httpCode = ThingSpeak.writeField(myChannelNumber, 1, valueA0, myWriteAPIKey);
+
+  if (httpCode == 200) {
+    Serial.println("Channel write successful.");
+  }
+  else {
+    Serial.println("Problem writing to channel. HTTP error code " + String(httpCode));
+  }
+
+  // Wait 20 seconds to update the channel again
+  delay(20000);
+}
---
+++ ESP8266/Analog/secret.cpp
@@ -0,0 +1,8 @@
+// Private credentials 
+// Connection details
+
+#define SECRET_SSID "MySSID"                                // replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"                            // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 123546                                 // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"                           // replace XYZ with your channel write API Key
